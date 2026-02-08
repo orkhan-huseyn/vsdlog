08ff30f14528bf1ed77ca7e574b8cb0cd87e8cf6
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2026-02-08 17:01:08 +0400

    Add read method and explanatory comments

--- internal/log/store.go
+++ internal/log/store.go
@@ -12,9 +12,14 @@ var (
 )
 
 const (
-	lenWidth = 8
+	// length of the record is appended to the buffer
+	// then the actual contents of the record is written
+	// because while reading, we'll need to know how many bytes to read
+	// length of the record is 64 bit unsigned integer (so 8 bytes)
+	headerSizeBytes = 8
 )
 
+// store is just a wrapper around os.File
 type store struct {
 	*os.File
 	mu   sync.Mutex
@@ -39,18 +44,47 @@ func (s *store) Append(record []byte) (uint64, uint64, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
+	// write to the end of the file
 	pos := s.size
+
+	// write length of the record (8 bytes) before the content
 	if err := binary.Write(s.buf, enc, uint64(len(record))); err != nil {
 		return 0, 0, err
 	}
 
+	// then write the actual content
 	w, err := s.buf.Write(record)
 	if err != nil {
 		return 0, 0, err
 	}
 
-	w += lenWidth
+	// total written bytes = bytesWritten + header size
+	w += headerSizeBytes
 	s.size += uint64(w)
 
 	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	// read the length of the content
+	// to know how many bytes we need to read
+	header := make([]byte, headerSizeBytes)
+	if _, err := s.File.ReadAt(header, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	// read the actual contents
+	contents := make([]byte, enc.Uint64(header))
+	if _, err := s.File.ReadAt(contents, int64(pos+headerSizeBytes)); err != nil {
+		return nil, err
+	}
+
+	return contents, nil
 }
